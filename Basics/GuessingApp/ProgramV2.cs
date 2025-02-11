7f923ff8527676fb97c7ed88fac65732e580198b
FardadRahmani <rahmanifardad@yahoo.de>
2025-02-11 11:28:05 +0100

    rpgGame

---
+++ Basics/GuessingApp/ProgramV2.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello, Fardad");
+
+Random random = new Random();
+
+int secretNumber = random.Next(1, 101);
+int userGuess = 0;
+int counter = 0;
+
+
+
+while (userGuess != secretNumber)
+{
+    counter++;
+    Console.WriteLine("Enter your Guess");
+    userGuess = int.Parse(Console.ReadLine());
+
+    if (userGuess < secretNumber)
+    {
+        Console.WriteLine("Too Low! Try again.");
+
+    } else if (userGuess > secretNumber)
+    {
+        Console.WriteLine("Too High! Try again.");
+        
+    } else
+    {
+        Console.WriteLine("Correct! Well Done! It took you " + counter + " tries.");
+    }
+}
+
+
+Console.ReadKey();
